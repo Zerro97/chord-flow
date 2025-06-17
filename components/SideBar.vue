08ef7e19dfe7f9cfb9c7567c2699ac512d179d76
Huibeom Kim <34781112+Zerro97@users.noreply.github.com>
2025-06-17 22:17:36 +0900

    feat: added more notes to key & increased menu width

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -7,7 +7,7 @@ const bpm = ref(100)
 const time_signatures = ref(["4/4", "3/4", "2/4", "6/8"])
 const current_time_signature = ref("4/4")
 
-const keys = ref(["C", "D", "E", "F", "G", "A", "B"])
+const keys = ref(note)
 const current_key = ref("C")
 
 const scales = ref(["major", "natural_minor"])
@@ -18,15 +18,18 @@ const current_notes = computed(() => get_scale_notes(current_key.value, current_
 </script>
 
 <template>
-  <div class="flex flex-col gap-y-6 w-[300px] px-4 py-4 bg-zinc-900 border-r border-zinc-800">
+  <div class="flex flex-col gap-y-2 w-[300px] px-4 py-4 bg-zinc-900 border-r border-zinc-800">
+    <p class="font-bold">Tempo</p>
     <UFormField label="BPM">
       <UInputNumber v-model="bpm" :min="0" :max="200" variant="subtle"/>
     </UFormField>
     <UFormField label="Time Signature">
       <URadioGroup v-model="current_time_signature" :items="time_signatures" />
     </UFormField>
+    <USeparator class="my-2" />
+    <p class="font-bold">Chord</p>
     <UFormField label="Key">
-      <USelect v-model="current_key" :items="keys" variant="subtle"/>
+      <USelect v-model="current_key" :items="keys" variant="subtle" class="min-w-[80px]"/>
     </UFormField>
     <UFormField label="Scale">
       <USelect v-model="current_scale" :items="scales" variant="subtle"/>
