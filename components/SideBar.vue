d90810763308213a428d008d578399fd812e7c20
Huibeom Kim <34781112+Zerro97@users.noreply.github.com>
2025-06-17 21:10:03 +0900

    feat: added controllers

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,12 +1,29 @@
 <script setup lang="ts">
+const bpm = ref(100)
 
+const time_signatures = ref(["4/4", "3/4", "2/4", "6/8"])
+const current_time_signature = ref("4/4")
+
+const keys = ref(["C", "D", "E", "F", "G", "A", "B"])
+const current_key = ref("C")
+
+const scales = ref(["Major", "Minor"])
+const current_scale = ref("Major")
 </script>
 
 <template>
-  <div class="flex flex-col h-screen w-[280px] px-4 py-2 bg-zinc-900 border-r border-zinc-800">
-    <p>Beat</p>
-    <p>Time Signature</p>
-    <p>Key</p>
-    <p>Scale</p>
+  <div class="flex flex-col gap-y-6 h-screen w-[280px] px-4 py-2 bg-zinc-900 border-r border-zinc-800">
+    <UFormField label="BPM">
+      <UInputNumber v-model="bpm" :min="0" :max="200"/>
+    </UFormField>
+    <UFormField label="Time Signature">
+      <URadioGroup v-model="current_time_signature" :items="time_signatures" />
+    </UFormField>
+    <UFormField label="Key">
+      <USelect v-model="current_key" :items="keys"/>
+    </UFormField>
+    <UFormField label="Scale">
+      <USelect v-model="current_scale" :items="scales"/>
+    </UFormField>
   </div>
 </template>
