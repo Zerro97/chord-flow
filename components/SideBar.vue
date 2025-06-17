665a55eae5d924d11feac711d5eb8cc38cc4cc53
Huibeom Kim <34781112+Zerro97@users.noreply.github.com>
2025-06-17 20:57:02 +0900

    feat: added sidebar & commented setInteval

---
+++ components/SideBar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col h-screen w-[280px] px-4 py-2 bg-zinc-900 border-r border-zinc-800">
+    <p>Beat</p>
+    <p>Time Signature</p>
+    <p>Key</p>
+    <p>Scale</p>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,18 @@
 const chords = ["C", "D", "E", "F", "G", "A", "B"]
 const chosen_chord = ref("C");
 
-setInterval(() => {
-  chosen_chord.value = chords[Math.floor(Math.random() * chords.length)];
-}, 1000);
+// setInterval(() => {
+//   chosen_chord.value = chords[Math.floor(Math.random() * chords.length)];
+// }, 1000);
 </script>
 
 <template>
-  <div class="bg-black w-screen h-screen flex justify-center items-center">
-    <div class="bg-zinc-900 rounded-2xl px-10 py-6">
-      <p>{{ chosen_chord }}</p>
+  <div class="bg-black w-screen h-screen flex">
+    <SideBar />
+    <div class="flex justify-center items-center w-full h-full">
+      <div class="bg-zinc-900 rounded-2xl px-10 py-6">
+        <p>{{ chosen_chord }}</p>
+      </div>
     </div>
   </div>
 </template>
