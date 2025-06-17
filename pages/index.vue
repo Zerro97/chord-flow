295aaaee8a737eef3858f122cf757942b11e5d81
Huibeom Kim <34781112+Zerro97@users.noreply.github.com>
2025-06-17 20:28:37 +0900

    feat: showing random chord on interval

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
+const chords = ["C", "D", "E", "F", "G", "A", "B"]
+const chosen_chord = ref("C");
 
+setInterval(() => {
+  chosen_chord.value = chords[Math.floor(Math.random() * chords.length)];
+}, 1000);
 </script>
 
 <template>
-
+  <div class="bg-black w-screen h-screen flex justify-center items-center">
+    <div class="bg-zinc-900 rounded-2xl px-10 py-6">
+      <p>{{ chosen_chord }}</p>
+    </div>
+  </div>
 </template>
