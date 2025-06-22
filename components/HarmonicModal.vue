07f1ffb150cd6d0d644a0c528c153bd5571ae25a
Huibeom Kim <34781112+Zerro97@users.noreply.github.com>
2025-06-22 22:32:08 +0900

    feat: made sidebar filters collapsible & added chord progression filter

---
+++ components/HarmonicModal.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const current_chord_progression = defineModel<string>('current_chord_progression')
+const major_progressions = [
+  "cadence_authentic",
+  "perfect_cadence",
+  "plagal_cadence	",
+  "pop_progression",
+  "doo_wop",
+  "50s_ballad",
+  "circle_of_fifths",
+]
+const minor_progressions = [
+  "natural_minor_cadence",
+  "harmonic_minor",
+  "sad_loop",
+  "descending_bass",
+  "andalusian_cadence",
+]
+</script>
+
+<template>
+  <UModal>
+    <UButton label="Harmonic" color="neutral" variant="subtle" />
+
+    <template #content>
+      <div class="px-4 py-2">
+        <h1 class="font-bold text-2xl">Harmonic</h1>
+        <div class="flex">
+          <div class="flex flex-col w-full">
+            <p>Major</p>
+            <URadioGroup v-model="current_chord_progression" :items="major_progressions" />
+          </div>
+          <div class="flex flex-col w-full">
+            <p>Minor</p>
+            <URadioGroup v-model="current_chord_progression" :items="minor_progressions" />
+          </div>
+        </div>
+      </div>
+    </template>
+  </UModal>
+</template>
--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,42 +1,115 @@
 <script setup lang="ts">
+import * as Tone from "tone";
+
 // Selected value props
 const current_bpm = defineModel<number>('current_bpm')
 const current_time_signature = defineModel<string>('current_time_signature')
 const current_scale = defineModel<string>('current_scale')
 const current_key = defineModel<string>('current_key')
 const current_volume = defineModel<number>('current_volume')
+const current_chord_progression = defineModel<string>('current_chord_progression')
 
 // Filters
 const time_signatures = ref(["4/4", "3/4", "2/4", "6/8"])
 const keys = ref(note)
 const scales = ref(["major", "natural_minor"])
 const randomize = ref(true)
+
+
+// Apply changes
+watch(() => {
+  const transport = Tone.getTransport()
+
+  if(transport.bpm) {
+    transport.bpm.value = current_bpm.value;
+  }
+})
 </script>
 
 <template>
-  <div class="flex flex-col gap-y-2 w-[300px] px-4 py-4 bg-zinc-900 border-r border-zinc-800">
-    <p class="font-bold">Tempo</p>
-    <UFormField label="BPM">
-      <UInputNumber v-model="current_bpm" :min="0" :max="200" variant="subtle"/>
-    </UFormField>
-    <UFormField label="Time Signature">
-      <URadioGroup v-model="current_time_signature" :items="time_signatures" />
-    </UFormField>
-    <USeparator class="my-2" />
-    <p class="font-bold">Chord</p>
-    <UFormField label="Key">
-      <USelect v-model="current_key" :items="keys" variant="subtle" class="min-w-[80px]"/>
-    </UFormField>
-    <UFormField label="Scale">
-      <USelect v-model="current_scale" :items="scales" variant="subtle"/>
-    </UFormField>
-    <USeparator class="my-2" />
-    <p class="font-bold">Other</p>
-    <UFormField label="Randomize">
-      <USwitch v-model="randomize" />
-    </UFormField>
-    <UFormField :label="`Volume (${current_volume}%)`">
-      <USlider v-model="current_volume" class="pt-1" />
-    </UFormField> 
+  <div class="flex flex-col w-[300px] bg-zinc-900 border-r border-zinc-800">
+    <div class="border-b-1 border-zinc-800 px-5 py-2">
+      <p class="font-bold text-xl">Chord Flow</p>
+    </div>
+    <div class="overflow-y-auto">
+      <UCollapsible :default-open="true" class="flex flex-col border-b-1 border-zinc-800">
+        <UButton
+          class="group font-bold bg-zinc-900 ring-0 rounded-none px-5"
+          label="Tempo"
+          color="neutral"
+          variant="subtle"
+          trailing-icon="i-lucide-chevron-down"
+          :ui="{
+            trailingIcon: 'group-data-[state=open]:rotate-180 transition-transform duration-200'
+          }"
+          block
+        />
+        <template #content>
+          <div class="flex flex-col gap-y-3 px-5 pt-1 pb-3">
+            <UFormField label="BPM">
+              <UInputNumber v-model="current_bpm" :min="30" :max="250" variant="subtle"/>
+              <USlider v-model="current_bpm" :min="30" :max="250" variant="subtle" class="mt-3 mb-1"/>
+            </UFormField>
+            <UFormField label="Time Signature">
+              <URadioGroup v-model="current_time_signature" :items="time_signatures" />
+            </UFormField>
+          </div>
+        </template>
+      </UCollapsible>
+      <UCollapsible :default-open="true" class="flex flex-col border-b-1 border-zinc-800">
+        <UButton
+          class="group font-bold bg-zinc-900 ring-0 rounded-none px-5"
+          label="Chord"
+          color="neutral"
+          variant="subtle"
+          trailing-icon="i-lucide-chevron-down"
+          :ui="{
+            trailingIcon: 'group-data-[state=open]:rotate-180 transition-transform duration-200'
+          }"
+          block
+        />
+        <template #content>
+          <div class="flex flex-col gap-y-3 px-5 pt-1 pb-3">
+            <UFormField label="Key">
+              <USelect v-model="current_key" :items="keys" variant="subtle" class="min-w-[80px]"/>
+            </UFormField>
+            <UFormField label="Scale">
+              <USelect v-model="current_scale" :items="scales" variant="subtle"/>
+            </UFormField>
+            <UFormField label="Style">
+              <div class="flex flex-col">
+                <div class="">
+                  <UButton label="Melodic" />
+                </div>
+                <HarmonicModal v-model="current_chord_progression" />
+              </div>
+            </UFormField>
+          </div>
+        </template>
+      </UCollapsible>
+      <UCollapsible :default-open="true" class="flex flex-col border-b-1 border-zinc-800">
+        <UButton
+          class="group font-bold bg-zinc-900 ring-0 rounded-none px-5"
+          label="Other"
+          color="neutral"
+          variant="subtle"
+          trailing-icon="i-lucide-chevron-down"
+          :ui="{
+            trailingIcon: 'group-data-[state=open]:rotate-180 transition-transform duration-200'
+          }"
+          block
+        />
+        <template #content>
+          <div class="flex flex-col gap-y-3 px-5 pt-1 pb-3">
+            <UFormField label="Randomize">
+              <USwitch v-model="randomize" />
+            </UFormField>
+            <UFormField :label="`Volume (${current_volume} db)`">
+              <USlider v-model="current_volume" :min="0" :max="50" class="py-1" />
+            </UFormField> 
+          </div>
+        </template>
+      </UCollapsible>
+    </div>
   </div>
 </template>
